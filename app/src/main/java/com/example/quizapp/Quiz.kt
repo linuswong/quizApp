d157183da5ef95247e2fd13c7d649277b0bb2c4f
linuswong <linuswong9@gmail.com>
2022-10-20 12:09:35 -0700

    Update Quiz.kt

--- app/src/main/java/com/example/quizapp/Quiz.kt
+++ app/src/main/java/com/example/quizapp/Quiz.kt
@@ -1,33 +1,35 @@
 package com.example.quizapp
 
 class Quiz(val questions:List<Question>) {
-    
-    
-    var score=0
-    var counter=0
-    var currentQuestion: Question= questions.get(counter)
-    
 
-    fun getQuestion():String{
-        return currentQuestion.question
-    }
-    fun getAnswers():List<String>{
-        return currentQuestion.answers
-    }
-    fun getCorrect():String{
-        return currentQuestion.correct
-    }
-    
-    fun answerCorrect(choice: String): Boolean{
-        currentQuestion= questions.get(counter)
-        if (choice.equals(currentQuestion.correct)){
-        score++     
-        }
-     counter++
-     return choice.equals(currentQuestion.correct)
-    }
-    fun urDone(){
-     return counter+1==questions.size()   
-    }
+
+   var tScore=0
+   var counter=0
+   var currentQuestion: Question= questions.get(0)
+
+
+
+   fun getQuestion():String{
+       return currentQuestion.question
+   }
+   fun getAnswers():List<String>{
+       return currentQuestion.answers
+   }
+   fun getCorrect():String{
+       return currentQuestion.correct
+   }
+
+   fun answerCorrect(choice: String){
+       currentQuestion= questions.get(counter)
+       if (choice.equals(getCorrect())){
+           tScore++
+       }
+       counter++
+       if(!(counter==questions.size)){
+           currentQuestion = questions.get(counter)
+       }
+   }
+   fun urDone():Boolean{
+       return counter==questions.size}
 
 }
